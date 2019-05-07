e50fc876944b779426aaeac8d3f3d964b5ec1993
bmVsc29u <bmVsc29u@gmail.com>
2019-05-07 20:07:10 +0800

    fix: app spec error

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,37 +1,42 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { TestBed, async } from '@angular/core/testing';
+
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
+import { of } from 'rxjs';
+
+const mockHttp = {
+  get() { return of('# test\ncontent\na'); }
+};
+
 import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import { AppComponent as Tag } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       schemas: [NO_ERRORS_SCHEMA],
       imports: [
+        HttpClientTestingModule,
         RouterTestingModule
       ],
+      providers: [
+        { provide: HttpClient, useValue: mockHttp }
+      ],
       declarations: [
-        AppComponent
+        Tag
       ],
     }).compileComponents();
   }));
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(Tag);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'demoUtils'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it('should handle http.get', () => {
+    const fixture = TestBed.createComponent(Tag);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('demoUtils');
-  });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to demoUtils!');
+    expect(app.data.setup).toEqual({ title: 'test', content: 'content\na' });
   });
 });
